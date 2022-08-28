b9d7e1f118c854ebf9020d2b8fb571ee8d379d45
EneRgYCZ <catalin.zaharia2013@gmail.com>
2022-08-28 18:06:44 +0300

    Refactor for the profile setup

--- lib/screens/profile/profile_setup_screen.dart
+++ lib/screens/profile/profile_setup_screen.dart
@@ -1,11 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:studhub/services/auth.dart';
-import 'package:studhub/services/firestore.dart';
-import 'package:flutter_tags_x/flutter_tags_x.dart';
-import 'package:introduction_screen/introduction_screen.dart';
+import 'package:studhub/widgets/profile/profile_setup_introduction_widget.dart';
 
 class ProfileSetupScreen extends StatefulWidget {
   const ProfileSetupScreen({Key? key}) : super(key: key);
@@ -15,149 +9,11 @@ class ProfileSetupScreen extends StatefulWidget {
 }
 
 class _ProfileSetupScreenState extends State<ProfileSetupScreen> {
-  String tags = '';
-
-  final TextEditingController _tagsController = TextEditingController();
-
-  bool isVerified = false;
-
-  void updateUserState() {
-    AuthService().user!.reload();
-    if (AuthService().user!.emailVerified) {
-      setState(() {
-        isVerified = true;
-      });
-    } else {
-      setState(() {
-        isVerified = false;
-      });
-    }
-  }
-
-  Timer? timer;
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(
-        const Duration(seconds: 1), (Timer t) => updateUserState());
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
-  }
-
-  List arrayOfTags = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Profile Setup")),
-      body: IntroductionScreen(
-        pages: [
-          PageViewModel(
-            titleWidget: const Padding(
-              padding: EdgeInsets.only(top: 20.0),
-              child: Text(
-                "What skills do you have?",
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
-            bodyWidget: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  arrayOfTags.isEmpty
-                      ? const SizedBox.shrink()
-                      : Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Row(
-                            children: [
-                              Tags(
-                                itemCount: arrayOfTags.length,
-                                itemBuilder: (int index) {
-                                  return Tooltip(
-                                    message: arrayOfTags[index],
-                                    child: ItemTags(
-                                      textActiveColor: Colors.white,
-                                      activeColor: Colors.blueGrey,
-                                      color: Colors.blueGrey,
-                                      textColor: Colors.white,
-                                      borderRadius: BorderRadius.circular(10),
-                                      index: index,
-                                      title: arrayOfTags[index],
-                                    ),
-                                  );
-                                },
-                              )
-                            ],
-                          ),
-                        ),
-                  Container(
-                    padding: const EdgeInsets.all(4),
-                    child: TextField(
-                      controller: _tagsController,
-                      decoration:
-                          const InputDecoration(labelText: "Add a tag..."),
-                      onChanged: (value) {
-                        setState(() {
-                          tags = value;
-                        });
-                      },
-                      onSubmitted: (value) {
-                        arrayOfTags.add(value);
-                        setState(() {
-                          _tagsController.clear();
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          PageViewModel(
-            titleWidget: const Padding(
-              padding: EdgeInsets.only(top: 20.0),
-              child: Text(
-                "Verification",
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
-            bodyWidget: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const Text("You now have to verify your account"),
-                TextButton(
-                    onPressed: () {
-                      AuthService().user!.sendEmailVerification();
-                      AuthService().user!.reload();
-                    },
-                    child: const Text("Send verification email")),
-                isVerified
-                    ? Lottie.network(
-                        "https://assets1.lottiefiles.com/packages/lf20_2mm5zqab.json",
-                        repeat: false,
-                      )
-                    : const SizedBox.shrink(),
-              ],
-            ),
-          ),
-        ],
-        showBackButton: true,
-        back: const Text("Back"),
-        done: isVerified ? const Text("Done") : const SizedBox.shrink(),
-        next: const Text("Next"),
-        onDone: () {
-          FirestoreService().updateSkills(arrayOfTags);
-          FirestoreService().updatIsVerified(isVerified);
-          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
-        },
-      ),
+      body: const ProfileSetupIntroductionWidget(),
     );
   }
 }
---
+++ lib/widgets/profile/profile_setup_introduction_widget.dart
@@ -0,0 +1,163 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tags_x/flutter_tags_x.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:studhub/services/auth.dart';
+import 'package:studhub/services/firestore.dart';
+
+class ProfileSetupIntroductionWidget extends StatefulWidget {
+  const ProfileSetupIntroductionWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileSetupIntroductionWidget> createState() =>
+      _ProfileSetupIntroductionWidgetState();
+}
+
+class _ProfileSetupIntroductionWidgetState
+    extends State<ProfileSetupIntroductionWidget> {
+  String tags = '';
+
+  final TextEditingController _tagsController = TextEditingController();
+
+  bool isVerified = false;
+
+  void updateUserState() {
+    AuthService().user!.reload();
+    if (AuthService().user!.emailVerified) {
+      setState(() {
+        isVerified = true;
+      });
+    } else {
+      setState(() {
+        isVerified = false;
+      });
+    }
+  }
+
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(
+        const Duration(seconds: 1), (Timer t) => updateUserState());
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  List arrayOfTags = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          titleWidget: const Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: Text(
+              "What skills do you have?",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          bodyWidget: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                arrayOfTags.isEmpty
+                    ? const SizedBox.shrink()
+                    : Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Row(
+                          children: [
+                            Tags(
+                              itemCount: arrayOfTags.length,
+                              itemBuilder: (int index) {
+                                return Tooltip(
+                                  message: arrayOfTags[index],
+                                  child: ItemTags(
+                                    textActiveColor: Colors.white,
+                                    activeColor: Colors.blueGrey,
+                                    color: Colors.blueGrey,
+                                    textColor: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    index: index,
+                                    title: arrayOfTags[index],
+                                  ),
+                                );
+                              },
+                            )
+                          ],
+                        ),
+                      ),
+                Container(
+                  padding: const EdgeInsets.all(4),
+                  child: TextField(
+                    controller: _tagsController,
+                    decoration:
+                        const InputDecoration(labelText: "Add a tag..."),
+                    onChanged: (value) {
+                      setState(() {
+                        tags = value;
+                      });
+                    },
+                    onSubmitted: (value) {
+                      arrayOfTags.add(value);
+                      setState(() {
+                        _tagsController.clear();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        PageViewModel(
+          titleWidget: const Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Verification",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          bodyWidget: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text("You now have to verify your account"),
+              TextButton(
+                  onPressed: () {
+                    AuthService().user!.sendEmailVerification();
+                    AuthService().user!.reload();
+                  },
+                  child: const Text("Send verification email")),
+              isVerified
+                  ? Lottie.network(
+                      "https://assets1.lottiefiles.com/packages/lf20_2mm5zqab.json",
+                      repeat: false,
+                    )
+                  : const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      ],
+      showBackButton: true,
+      back: const Text("Back"),
+      done: isVerified ? const Text("Done") : const SizedBox.shrink(),
+      next: const Text("Next"),
+      onDone: () {
+        FirestoreService().updateSkills(arrayOfTags);
+        FirestoreService().updatIsVerified(isVerified);
+        Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+      },
+    );
+  }
+}
